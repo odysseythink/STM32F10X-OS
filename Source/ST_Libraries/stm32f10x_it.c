49b7819ed112b06a3698a030eeee57143fa80a23
ranwei <ranwei693532@163.com>
2016-03-28 09:45:53 +0800

    ranwei <ranwei693532@163.com>:Start of the project

---
+++ Source/ST_Libraries/stm32f10x_it.c
@@ -0,0 +1,169 @@
+/*******************************************************************************
+        (c) COPYRIGHT 2010-2018 by Efficient Systems, Inc.    
+                          All rights reserved.
+    
+       This software is confidential and proprietary to Efficient 
+     Systems, Inc.  No part of this software may be reproduced,    
+     stored, transmitted, disclosed or used in any form or by any means
+     other than as expressly provided by the written license agreement    
+     between Efficient Systems and its licensee.
+ FileName    : stm32f10x_it.c
+ Author      : rw
+ Version     : 
+ Date        : 2016/3/27 17:53:18:897
+ Description : 
+ Others      : 
+
+ History      :
+  1.Date         -- 2016/3/27 17:53:18:897
+    Author       -- rw
+    Modification -- Created file
+
+*******************************************************************************/
+    
+#define  STM32F10X_IT_GLOBAL
+
+/* includes-------------------------------------------------------------------*/
+#include "stm32f10x_it.h"
+#include "OS.h"
+
+    
+/* Private typedef&macro&definde----------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/* External functions --------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+
+
+
+
+/******************************************************************************/
+/*            Cortex-M3 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSV_Handler exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+    OS_Time_Update();
+}
+
+/******************************************************************************/
+/*                 STM32F10x Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f10x_xx.s).                                            */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+
+    
+/**************** (C) COPYRIGHT 2010-2018 Efficient *****END OF FILE***********/
